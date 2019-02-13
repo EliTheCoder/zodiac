44958ebfac530c94dca6199a0d6e919616181c79
EliTheCoder <ex.games.org@gmail.com>
2019-02-12 18:04:58 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const SHA512 = require("crypto-js/SHA512");
+
+class Block {
+  constructor(index, timestamp, data, previousHash = '') {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = '';
+  }
+  calculateHash() {
+    return SHA512(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, new Date(), "Genesis", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+}
+
+let zodiac = new Blockchain();
+zodiac.addBlock(new Block(zodiac.getLatestBlock().index + 1, new Date(), {
+  amount: 10
+}));
+
+console.log(zodiac.chain);
